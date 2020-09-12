d6ca5376a1ec666467a346ce24c0e8703984343a
Adityaraj1711 <adityamailbox0@gmail.com>
2020-09-13 04:55:38 +0530

    added global and development dependencies for the node api

--- todo-api/server.js
+++ todo-api/server.js
@@ -1,1 +1,13 @@
-console.log("Hello Node");
+var http = require('http');
+
+http.createServer(function(request, response){
+    // 200 - success
+    // 400 - program error
+    // 500 - server error
+    response.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
+
+    response.end("Hello nodemon");
+
+}).listen(3000, console.log("server is running on port 3000")); 
