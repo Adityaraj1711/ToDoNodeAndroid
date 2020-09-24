7222c333056937269c966cf3bef9d03ec3b2c645
Adityaraj1711 <adityamailbox0@gmail.com>
2020-09-24 22:15:24 +0530

    added register activity layout and dependencies

--- todo-android/app/src/main/java/com/pictionary/todoapp/RegisterActivity.kt
+++ todo-android/app/src/main/java/com/pictionary/todoapp/RegisterActivity.kt
@@ -6,16 +6,16 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 
-class LoginActivity : AppCompatActivity() {
-    lateinit var registerBtn: Button
+class RegisterActivity : AppCompatActivity() {
+    lateinit var loginBtn: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-        registerBtn = findViewById(R.id.registerBtn)
-        registerBtn.setOnClickListener(object : View.OnClickListener{
+        setContentView(R.layout.activity_register)
+        loginBtn = findViewById(R.id.loginBtn)
+        loginBtn.setOnClickListener(object : View.OnClickListener{
             override fun onClick(p0: View?) {
-                val intent: Intent = Intent(this@LoginActivity, RegisterActivity::class.java)
+                val intent: Intent = Intent(this@RegisterActivity, LoginActivity::class.java)
                 startActivity(intent)
             }
         })
