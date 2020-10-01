911af43dc2e40815c587594a5037f573fbfe2a0e
Adityaraj1711 <adityamailbox0@gmail.com>
2020-10-01 14:07:03 +0530

    added DELETE and PUT method for ToDo Api

--- todo-api/routes/todo.js
+++ todo-api/routes/todo.js
@@ -20,7 +20,7 @@ router.post('/', auth, async (req, res, next) => {
             todo: toDo,
             msg: 'successfully created'
         });
-    } 
+    }
     catch(error){
         next(error);
     }
@@ -42,5 +42,48 @@ router.get('/', auth, async(req, res, next) => {
         next(error);
     }
 })
+
+router.put('/:id', auth, async(req, res, next) =>{
+    try{
+        var params = req.params;
+        var post_id = params.id;
+        let todo = await Todo.findById(post_id);
+        if(!todo){
+            return res.status(400).json({success: false, msg: "Task does not exist"});
+        }
+        todo = await Todo.findByIdAndUpdate(post_id, req.body, {
+            new: true,
+            runValidators: true
+        });
+        if(!todo){
+            return res.status(400).json({success: false, msg: "Something went wrong"});
+        }
+        res.status(200).json({success: true, todos: todo ,msg: "Successfully updated"});
+
+    }
+    catch(error){
+        next(error);
+    }   
+})
+
+
+router.delete('/:id', async(req, res, next) =>{
+    try{
+        var params = req.params;
+        var post_id = params.id;
+        let todo = await Todo.findById(post_id);
+        if(!todo){
+            return res.status(400).json({success: false, msg: "Task does not exist"});
+        }
+        todo = await Todo.findByIdAndDelete(post_id);
+        
+        res.status(200).json({success: true, todos: todo ,msg: "Task Successfully deleted"});
+
+    }
+    catch(error){
+        next(error);
+    }
+})
+
 
 module.exports = router;
