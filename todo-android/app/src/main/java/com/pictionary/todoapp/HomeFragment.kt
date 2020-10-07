4ec0803007747620db5d0b7ab85aba21402b73e2
Adityaraj1711 <adityamailbox0@gmail.com>
2020-10-08 02:42:01 +0530

    added add notes custom dialog in home fragment

--- todo-android/app/src/main/java/com/pictionary/todoapp/HomeFragment.kt
+++ todo-android/app/src/main/java/com/pictionary/todoapp/HomeFragment.kt
@@ -1,59 +1,69 @@
 package com.pictionary.todoapp
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    lateinit var floatingActionButton: FloatingActionButton
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        floatingActionButton = view.findViewById(R.id.add_task_btn)
+        floatingActionButton.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(p0: View?) {
+                showAlertDialog()
+            }
+        })
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    private fun showAlertDialog() {
+        val inflater: LayoutInflater = layoutInflater
+        val alertLayout: View = inflater.inflate(R.layout.custom_dialog_layout, null)
+        val title : EditText = alertLayout.findViewById(R.id.title)
+        val description: EditText = alertLayout.findViewById(R.id.description)
+//        val alertDialog: AlertDialog = AlertDialog.Builder(activity).create()
+//        alertDialog.setView(alertLayout)
+//        alertDialog.setTitle("Add Task")
+//
+//
+//        alertDialog.show()
+
+        val dialog: AlertDialog = AlertDialog.Builder(activity)
+            .setView(alertLayout)
+            .setTitle("Add Task")
+            .setPositiveButton("Add", null)
+            .setNegativeButton("Cancel", null)
+            .create()
+        dialog.setOnShowListener(object : DialogInterface.OnShowListener{
+            override fun onShow(dialogInterface: DialogInterface) {
+                val button: Button = (dialog as AlertDialog).getButton(AlertDialog.BUTTON_POSITIVE)
+                button.setOnClickListener(object : View.OnClickListener{
+                    override fun onClick(p0: View?) {
+                        Toast.makeText(activity, "Positive Button Pressed", Toast.LENGTH_SHORT).show()
+                    }
+                })
             }
+        })
+        dialog.show()
     }
+
+
 }
